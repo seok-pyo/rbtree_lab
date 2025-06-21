bcc80b142654facd847ab5f05a9235ac4f5a20de
hongseokpyo <hongseokpyou@gmail.com>
2025-06-21 10:23:57 +0900

    feat: new_rbtree function

--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -2,43 +2,86 @@
 
 #include <stdlib.h>
 
-rbtree *new_rbtree(void) {
+// typedef enum
+// {
+//   RBTREE_BLACK,
+//   RBTREE_RED,
+// } color_t;
+
+// //
+// typedef int key_t;
+
+// //
+// typedef struct node_t
+// {
+//   color_t color;
+//   key_t key;
+//   struct node_t *parent, *left, *right;
+// } node_t;
+
+//
+// typedef struct
+// {
+//   node_t *root;
+//   node_t *nil;
+// } rbtree;
+
+rbtree *new_rbtree(void)
+{
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
+
+  node_t *nil_node = (node_t *)calloc(1, sizeof(node_t));
+
+  nil_node->left = nil_node->right = nil_node->parent = NULL;
+  nil_node->color = RBTREE_BLACK;
+
+  p->nil = nil_node;
+
+  p->root = p->nil;
+
+  // p->root = NULL;
+
   return p;
 }
 
-void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
-  free(t);
-}
+// void delete_rbtree(rbtree *t)
+// {
+//   // TODO: reclaim the tree nodes's memory
+//   free(t);
+// }
 
-node_t *rbtree_insert(rbtree *t, const key_t key) {
+node_t *rbtree_insert(rbtree *t, const key_t key)
+{
   // TODO: implement insert
   return t->root;
 }
 
-node_t *rbtree_find(const rbtree *t, const key_t key) {
-  // TODO: implement find
-  return t->root;
-}
+// node_t *rbtree_find(const rbtree *t, const key_t key)
+// {
+//   // TODO: implement find
+//   return t->root;
+// }
 
-node_t *rbtree_min(const rbtree *t) {
-  // TODO: implement find
-  return t->root;
-}
+// node_t *rbtree_min(const rbtree *t)
+// {
+//   // TODO: implement find
+//   return t->root;
+// }
 
-node_t *rbtree_max(const rbtree *t) {
-  // TODO: implement find
-  return t->root;
-}
+// node_t *rbtree_max(const rbtree *t)
+// {
+//   // TODO: implement find
+//   return t->root;
+// }
 
-int rbtree_erase(rbtree *t, node_t *p) {
-  // TODO: implement erase
-  return 0;
-}
+// int rbtree_erase(rbtree *t, node_t *p)
+// {
+//   // TODO: implement erase
+//   return 0;
+// }
 
-int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
-  // TODO: implement to_array
-  return 0;
-}
+// int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
+// {
+//   // TODO: implement to_array
+//   return 0;
+// }
